d725de004fb601ff9e8092160b4bc6a0d23b9699
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-04-11 08:35:08 -0300

    reogabi

---
+++ aula8cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using aula8cs.src;
+using System;
+
+namespace aula8cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("atividade classe");
+            Cachorro floquinho = new Cachorro("Floquinho", 5, "azul", "pequeno");
+
+            floquinho.Comer();
+        }
+    }
+}
---
+++ aula8cs/src/Cachorro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula8cs.src
+{
+    class Cachorro
+    {
+        public string Nome { get; set; }
+        public int Peso { get; set; }
+        public string Cor { get; set; }
+        public string Tamanho { get; set; }
+
+        public Cachorro(string _nome, int _peso, string _cor, string _tamanho)
+        {
+            Nome = _nome;
+            Peso = _peso;
+            Cor = _cor;
+            Tamanho = _tamanho;
+        }
+
+        public void Comer()
+        {
+            Console.WriteLine("comendo");
+            
+        }
+
+        public string Dormir()
+        { 
+            return "dormindo";
+        }
+             
+    }
+}
